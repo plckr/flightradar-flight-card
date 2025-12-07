0177551d2d865b63a79e9efff51bb6d918ace226
Ricardo Reis <ricardo@plckr.dev>
2025-12-07 22:41:51 +0000

    refactor rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -3,45 +3,52 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
+import { defineConfig } from 'rollup';
 
 const dev = !!process.env.ROLLUP_WATCH;
 
-const serveOptions = {
-  contentBase: ['./dist'],
-  host: '0.0.0.0',
-  port: 4000,
-  allowCrossOrigin: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-};
-
-export default {
+export default defineConfig(() => ({
   input: 'src/flightradar-flight-card.ts',
   output: {
     file: 'dist/flightradar-flight-card.js',
     format: 'es',
     inlineDynamicImports: true,
+    sourcemap: dev,
   },
-  plugins: [
-    resolve({
-      browser: true,
-    }),
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.json',
-      sourceMap: dev,
-      inlineSources: dev,
-    }),
-    dev
-      ? serve(serveOptions)
-      : terser({
-          format: {
-            comments: false,
-          },
-        }),
-  ],
+  plugins,
   watch: {
     clearScreen: false,
   },
-};
+}));
+
+const plugins = [
+  resolve({ browser: true }),
+  commonjs(),
+  typescript({
+    tsconfig: './tsconfig.json',
+    sourceMap: dev,
+    inlineSources: dev,
+  }),
+];
+
+if (dev) {
+  plugins.push(
+    serve({
+      contentBase: ['./dist'],
+      host: '0.0.0.0',
+      port: 4000,
+      allowCrossOrigin: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+  );
+} else {
+  plugins.push(
+    terser({
+      format: {
+        comments: false,
+      },
+    })
+  );
+}
